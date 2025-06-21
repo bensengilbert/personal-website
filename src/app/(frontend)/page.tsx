76ebd043c7b166cdbde35650d3af58ece32bd9f5
bensen <agben2410@gmail.com>
2025-06-21 16:59:02 +0530

    test commit after change in repo

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -5,13 +5,24 @@ import { components } from "@/sanity/PortableTextComponents";
 import { PortableText, PortableTextBlock } from "next-sanity";
 
 export default async function Page() {
-  const {data: landingPageData} = await sanityFetch({query: LANDING_PAGE_QUERY});
-  
+  const { data: landingPageData } = await sanityFetch({
+    query: LANDING_PAGE_QUERY,
+  });
+
+  console.log(
+    "added this log just for testing purpose, will remove this in next release!",
+  );
+
   return (
-     <section className="flex min-h-screen items-center justify-center font-apple">
+    <section className="flex min-h-screen items-center justify-center font-apple">
       <div className="max-w-2xl space-y-6 text-center px-4">
-        <Title>{landingPageData?.greetings} {landingPageData?.title}</Title>
-        <PortableText value={landingPageData?.summary as PortableTextBlock[]} components={components} />
+        <Title>
+          {landingPageData?.greetings} {landingPageData?.title}
+        </Title>
+        <PortableText
+          value={landingPageData?.summary as PortableTextBlock[]}
+          components={components}
+        />
       </div>
     </section>
   );
